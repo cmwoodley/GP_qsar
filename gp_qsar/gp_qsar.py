5b251cdb0820ca4f937b062e152e792b84ca6b3d
cmwoodley <christopher.woodley@hotmail.co.uk>
2024-09-03 12:38:36 +0100

    format with black

--- gp_qsar/gp_qsar.py
+++ gp_qsar/gp_qsar.py
@@ -8,7 +8,12 @@ from copy import deepcopy
 from scipy.special import ndtr
 from scipy.stats import norm
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, Matern, WhiteKernel, ConstantKernel as C
+from sklearn.gaussian_process.kernels import (
+    RBF,
+    Matern,
+    WhiteKernel,
+    ConstantKernel as C,
+)
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.model_selection import GridSearchCV
@@ -16,49 +21,62 @@ from sklearn.model_selection import GridSearchCV
 ### Define Acquisition functions
 # Equations taken from https://github.com/modAL-python/modAL
 
+
 def PI(mean, std, max_val, tradeoff):
-    return ndtr((mean - max_val - tradeoff)/std)
+    return ndtr((mean - max_val - tradeoff) / std)
 
 
 def EI(mean, std, max_val, tradeoff):
     z = (mean - max_val - tradeoff) / std
-    return (mean - max_val - tradeoff)*ndtr(z) + std*norm.pdf(z)
+    return (mean - max_val - tradeoff) * ndtr(z) + std * norm.pdf(z)
 
 
 def UCB(mean, std, kappa):
-    return mean + kappa*std
+    return mean + kappa * std
+
 
 ### Helper functions for feature selection
+
 
 def get_all_descriptors(smiles):
     mols = [Chem.MolFromSmiles(smi) for smi in smiles]
     descriptors = {}
-    descriptors["ECFP"] = np.array([AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048) for mol in mols])
-    descriptors["FCFP"] = np.array([AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048, useFeatures=True) for mol in mols])
-    descriptors["Physchem"] = np.array([
-    [
-        Descriptors.MolWt(mol),
-        Descriptors.MolLogP(mol),
-        Descriptors.NumHDonors(mol),
-        Descriptors.NumHAcceptors(mol),
-        Descriptors.TPSA(mol),
-        Descriptors.NumRotatableBonds(mol),
-        Descriptors.NumAromaticRings(mol),
-        Descriptors.FractionCSP3(mol),
-        Descriptors.BalabanJ(mol),
-        Descriptors.Chi0n(mol),
-        Descriptors.Chi1n(mol),
-        Descriptors.Chi2n(mol),
-        Descriptors.Kappa1(mol),
-        Descriptors.Kappa2(mol),
-        Descriptors.Kappa3(mol),
-        Descriptors.LabuteASA(mol),
-        Descriptors.HallKierAlpha(mol),
-        EState.EState_VSA.EState_VSA1(mol), #EState_VSA bin 1
-    ]
-    for mol in  mols
-])
+    descriptors["ECFP"] = np.array(
+        [AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048) for mol in mols]
+    )
+    descriptors["FCFP"] = np.array(
+        [
+            AllChem.GetMorganFingerprintAsBitVect(mol, 2, 2048, useFeatures=True)
+            for mol in mols
+        ]
+    )
+    descriptors["Physchem"] = np.array(
+        [
+            [
+                Descriptors.MolWt(mol),
+                Descriptors.MolLogP(mol),
+                Descriptors.NumHDonors(mol),
+                Descriptors.NumHAcceptors(mol),
+                Descriptors.TPSA(mol),
+                Descriptors.NumRotatableBonds(mol),
+                Descriptors.NumAromaticRings(mol),
+                Descriptors.FractionCSP3(mol),
+                Descriptors.BalabanJ(mol),
+                Descriptors.Chi0n(mol),
+                Descriptors.Chi1n(mol),
+                Descriptors.Chi2n(mol),
+                Descriptors.Kappa1(mol),
+                Descriptors.Kappa2(mol),
+                Descriptors.Kappa3(mol),
+                Descriptors.LabuteASA(mol),
+                Descriptors.HallKierAlpha(mol),
+                EState.EState_VSA.EState_VSA1(mol),  # EState_VSA bin 1
+            ]
+            for mol in mols
+        ]
+    )
     return descriptors
+
 
 def var_corr_scaler(X, VT, scaler):
     X = VT.fit_transform(X)
@@ -68,6 +86,7 @@ def var_corr_scaler(X, VT, scaler):
     X = np.delete(X, to_drop, axis=1)
     X = scaler.fit_transform(X)
     return X, VT, scaler, to_drop
+
 
 class Descriptor:
     def __init__(self, VT, scaler, to_drop, features):
@@ -84,18 +103,21 @@ class Descriptor:
         fps = self.scaler.transform(fps)
         return fps
 
+
 class GP_qsar:
     def __init__(self, train_smiles, train_y):
         self.train_smiles = train_smiles
         self.y = train_y
         self.X = None
-        self.predictor = GaussianProcessRegressor(n_restarts_optimizer=10, random_state=42)
+        self.predictor = GaussianProcessRegressor(
+            n_restarts_optimizer=10, random_state=42
+        )
         self.features = []
         self.descriptor = None
         self.metadata = {
-                        "model_name": "GP_qsar",
-                        "version": 1.0,
-                    }
+            "model_name": "GP_qsar",
+            "version": 1.0,
+        }
 
         # Preprocessing tools
         self.VT = VarianceThreshold(threshold=0.0)
@@ -108,54 +130,71 @@ class GP_qsar:
         self.feature_dict = get_all_descriptors(self.train_smiles)
 
     def fit_tune_model(self):
-        self.predictor, self.X, self.features, self.VT, self.scaler, to_drop = tune_model(self.predictor, self.feature_dict, self.y, self.VT, self.scaler)
+        (
+            self.predictor,
+            self.X,
+            self.features,
+            self.VT,
+            self.scaler,
+            to_drop,
+        ) = tune_model(self.predictor, self.feature_dict, self.y, self.VT, self.scaler)
         self.descriptor = Descriptor(self.VT, self.scaler, to_drop, self.features)
-        
+
     def predict_from_smiles(self, smiles, uncert=False):
         if isinstance(smiles, str):
             smiles = [smiles]
-        
+
         test_fps = self.descriptor.calculate_from_smi(smiles)
         if uncert:
             predictions, std = self.predictor.predict(test_fps, return_std=True)
             return predictions, std
-        else: 
+        else:
             predictions = self.predictor.predict(test_fps, return_std=False)
             return predictions
-        
-    def evaluate_acquisition_functions(self, smiles, acquisition_function='EI', y_max = None, kappa=1., tradeoff = 0.):
+
+    def evaluate_acquisition_functions(
+        self, smiles, acquisition_function="EI", y_max=None, kappa=1.0, tradeoff=0.0
+    ):
         if y_max == None:
             y_max = np.max(self.y)
 
         if isinstance(smiles, str):
             smiles = [smiles]
-        
+
         # Calculate descriptors for input SMILES
         x = self.descriptor.calculate_from_smi(smiles)
-        
+
         # Get GP predictions and uncertainties
         predictions, std = self.predictor.predict(x, return_std=True)
 
         # Evaluate acquisition functions
-        if acquisition_function == 'EI':
+        if acquisition_function == "EI":
             ei = EI(predictions, std, y_max, tradeoff)
             return ei
-        
-        elif acquisition_function == 'PI':
+
+        elif acquisition_function == "PI":
             pi = PI(predictions, std, y_max, tradeoff)
             return pi
 
-        elif acquisition_function == 'UCB':
+        elif acquisition_function == "UCB":
             ucb = UCB(predictions, std, kappa)
             return ucb
-        
+
         else:
-            raise ValueError("Unsupported acquisition function. Choose from 'EI', 'PI', or 'UCB'.")
+            raise ValueError(
+                "Unsupported acquisition function. Choose from 'EI', 'PI', or 'UCB'."
+            )
+
 
 def tune_model(gp, feature_dict, y, VT, scaler):
-    feature_combinations = list(itertools.chain.from_iterable(itertools.combinations(list(feature_dict.keys()), r) for r in range(1, len(feature_dict.keys())+1)))
+    feature_combinations = list(
+        itertools.chain.from_iterable(
+            itertools.combinations(list(feature_dict.keys()), r)
+            for r in range(1, len(feature_dict.keys()) + 1)
+        )
+    )
     best_models = []
-    best_score = []    
+    best_score = []
 
     for features in feature_combinations:
         gp_temp = deepcopy(gp)
@@ -177,28 +216,34 @@ def tune_model(gp, feature_dict, y, VT, scaler):
 
     return gp, X, best_features, VT, scaler, to_drop
 
+
 def tune_hyperparameters(gp, X, y):
     kernels = [
         # RBF Kernel with more conservative bounds
         C(1.0, (0.1, 10.0)) * RBF(1.0, (0.1, 10.0)) + WhiteKernel(),
-        C(1.0, (0.1, 10.0)) * RBF(length_scale=1.0, length_scale_bounds=(0.1, 10.0)) + WhiteKernel(),
-
+        C(1.0, (0.1, 10.0)) * RBF(length_scale=1.0, length_scale_bounds=(0.1, 10.0))
+        + WhiteKernel(),
         # Matern Kernel with nu=1.5, more conservative bounds
-        C(1.0, (0.1, 10.0)) * Matern(length_scale=1.0, nu=1.5, length_scale_bounds=(0.1, 10.0)) + WhiteKernel(),
-        C(1.0, (0.1, 10.0)) * Matern(length_scale=1.0, nu=2.5, length_scale_bounds=(0.1, 10.0)) + WhiteKernel(),
-
+        C(1.0, (0.1, 10.0))
+        * Matern(length_scale=1.0, nu=1.5, length_scale_bounds=(0.1, 10.0))
+        + WhiteKernel(),
+        C(1.0, (0.1, 10.0))
+        * Matern(length_scale=1.0, nu=2.5, length_scale_bounds=(0.1, 10.0))
+        + WhiteKernel(),
         # Simpler Kernel Combinations with conservative bounds
         C(1.0, (0.1, 10.0)) * WhiteKernel(),
         C(1.0, (0.1, 10.0)) * RBF(1.0, (0.1, 10.0)),
-        C(1.0, (0.1, 10.0)) * Matern(length_scale=1.0, nu=1.5)
+        C(1.0, (0.1, 10.0)) * Matern(length_scale=1.0, nu=1.5),
     ]
 
     param_grid = {
         "alpha": [1e-2, 1e-1, 1e0, 1e1],  # More regularization to avoid overfitting
-        "kernel": kernels
+        "kernel": kernels,
     }
 
-    grid_search = GridSearchCV(gp, param_grid=param_grid, cv=10, n_jobs=-1, scoring="neg_mean_squared_error") 
+    grid_search = GridSearchCV(
+        gp, param_grid=param_grid, cv=10, n_jobs=-1, scoring="neg_mean_squared_error"
+    )
     grid_search.fit(X, y)
-    
+
     return grid_search.best_estimator_, grid_search.best_score_
--- gp_qsar/utils.py
+++ gp_qsar/utils.py
@@ -8,15 +8,18 @@ from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import KBinsDiscretizer
 
+
 def stratified_split(smiles, y, random_state, test_size, n_bins):
     """
     Stratified split by binning target variable and random sampling from each bin.
     """
-    binner = KBinsDiscretizer(n_bins=n_bins, encode='ordinal', strategy='uniform')
+    binner = KBinsDiscretizer(n_bins=n_bins, encode="ordinal", strategy="uniform")
     y = np.array(y).reshape(-1, 1)
     y_binned = binner.fit_transform(y).astype(int).ravel()
 
-    splitter = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=random_state)
+    splitter = StratifiedShuffleSplit(
+        n_splits=1, test_size=test_size, random_state=random_state
+    )
     train_index, test_index = next(splitter.split(y, y_binned))
 
     smiles_tr, smiles_te = np.array(smiles)[train_index], np.array(smiles)[test_index]
@@ -24,22 +27,24 @@ def stratified_split(smiles, y, random_state, test_size, n_bins):
 
     return smiles_tr, smiles_te, np.ravel(y_tr), np.ravel(y_te)
 
-def butina_cluster(mol_list,cutoff=0.3):
+
+def butina_cluster(mol_list, cutoff=0.3):
     """
     Butina clustering algorithm, taken from https://github.com/PatWalters/workshop/blob/master/clustering/taylor_butina.ipynb
     """
     fp_list = [rdmd.GetMorganFingerprintAsBitVect(m, 3, nBits=2048) for m in mol_list]
     dists = []
     nfps = len(fp_list)
-    for i in range(1,nfps):
-        sims = DataStructs.BulkTanimotoSimilarity(fp_list[i],fp_list[:i])
-        dists.extend([1-x for x in sims])
-    mol_clusters = Butina.ClusterData(dists,nfps,cutoff,isDistData=True)
-    cluster_id_list = [0]*nfps
-    for idx,cluster in enumerate(mol_clusters,1):
+    for i in range(1, nfps):
+        sims = DataStructs.BulkTanimotoSimilarity(fp_list[i], fp_list[:i])
+        dists.extend([1 - x for x in sims])
+    mol_clusters = Butina.ClusterData(dists, nfps, cutoff, isDistData=True)
+    cluster_id_list = [0] * nfps
+    for idx, cluster in enumerate(mol_clusters, 1):
         for member in cluster:
             cluster_id_list[member] = idx
     return cluster_id_list, fp_list
+
 
 def scaffold_split(smiles, y, random_state, test_size, cutoff):
     """
@@ -57,19 +62,27 @@ def scaffold_split(smiles, y, random_state, test_size, cutoff):
 
     train_index, test_index = [], []
 
-    singleton_clusters = [cluster for cluster, idx_list in cluster_dict.items() if len(idx_list) == 1]
+    singleton_clusters = [
+        cluster for cluster, idx_list in cluster_dict.items() if len(idx_list) == 1
+    ]
     # Raise error if all singleton clusters - to do: default to clustering with set number of clusters
     if len(singleton_clusters) == len(smiles):
-        raise ValueError("Clustering produced all singleton clusters. Try increasing cutoff")
+        raise ValueError(
+            "Clustering produced all singleton clusters. Try increasing cutoff"
+        )
     train_index.extend([cluster_dict[cluster][0] for cluster in singleton_clusters])
 
-    remaining_clusters = [cluster for cluster in cluster_dict if cluster not in singleton_clusters]
+    remaining_clusters = [
+        cluster for cluster in cluster_dict if cluster not in singleton_clusters
+    ]
     remaining_examples = len(smiles) - len(train_index)
     updated_split = test_size * len(smiles) / remaining_examples
 
     for cluster in remaining_clusters:
         idx_list = cluster_dict[cluster]
-        clust_train, clust_test = train_test_split(idx_list, test_size=updated_split, random_state=random_state)
+        clust_train, clust_test = train_test_split(
+            idx_list, test_size=updated_split, random_state=random_state
+        )
         train_index.extend(clust_train)
         test_index.extend(clust_test)
 
@@ -79,21 +92,28 @@ def scaffold_split(smiles, y, random_state, test_size, cutoff):
     y_te = np.array(y)[test_index]
     return smiles_tr, smiles_te, y_tr, y_te
 
-def splitter(smiles, y, method="random", random_state=42, test_size=0.2, n_bins=5, cutoff=0.3):
+
+def splitter(
+    smiles, y, method="random", random_state=42, test_size=0.2, n_bins=5, cutoff=0.3
+):
     """
     Split a set of smiles and associated target variables by various methods.
     """
     if method.lower() == "random":
-        return train_test_split(smiles, y, test_size=test_size, random_state=random_state)
+        return train_test_split(
+            smiles, y, test_size=test_size, random_state=random_state
+        )
 
     if method.lower() == "stratified":
         if n_bins is None:
             raise ValueError("n_bins must be specified for stratified split")
         return stratified_split(smiles, y, random_state, test_size, n_bins)
-    
+
     if method.lower() == "scaffold":
         if cutoff is None:
             raise ValueError("cutoff must be specified for scaffold split")
         return scaffold_split(smiles, y, random_state, test_size, cutoff)
 
-    raise ValueError(f"Unknown method '{method}'. Supported methods are: 'random', 'stratified', 'scaffold'")
+    raise ValueError(
+        f"Unknown method '{method}'. Supported methods are: 'random', 'stratified', 'scaffold'"
+    )
